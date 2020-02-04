464fb68a583981a8eda30ab2addba73495769b90
Sorrelljs <[sjsohota@gmail.com]>
2020-02-03 19:34:42 -0800

    updated about page, implemented font-family and advanced styling

--- themes/QuotesOnDev/front-page.php
+++ themes/QuotesOnDev/front-page.php
@@ -12,8 +12,16 @@
 
 
 
+<!-- DIVS CLASS -->
 
+<div class = "entry-content">
 
+</div>
+
+<div class = "entry-meta">
+<h3 class = "author-name-content"></h3>
+
+</div>
 <section class = "button-container">
 <button class = "button-click" type="button" id="new-quote-button">Show Me Another</button>
 </section>
--- themes/QuotesOnDev/js/api.js
+++ themes/QuotesOnDev/js/api.js
@@ -6,7 +6,7 @@
         event.preventDefault();
         $.ajax({
             method: "get",
-            url: apiVars.rest_url + "/wp/v2/posts?filter[posts_per_page]=1&filter[orderby]=rand",
+            url: apiVars.rest_url + "wp/v2/posts?filter[posts_per_page]=1&filter[orderby]=rand",
 
 
             beforeSend: function (xhr) {
@@ -15,8 +15,21 @@
         }).done(function (data) {
             console.log(data);
             // alert("Success! Comments are closed for this post.");
+            const author = data[0].title.rendered
+            const quote = data[0].content.rendered
+
+            // ***** appending data ***///
+            console.log(quote);
+            $(".entry-content").html(quote);
+            $(".entry-content p").addClass('quote-text');
+            $(".author-name-content").text(author);
+
         });
+
     });
+    $(".button-click").click()
+
 })(jQuery);
 
 // });
+
